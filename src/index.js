212667b78a46ac7ff5f629cb555676a6261cf7d0
ianespanto <ianespanto@users.noreply.github.com>
2022-03-15 18:34:52 -0700

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './sass/style.scss';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+
+// console fun
+const consoleStyle = (background, color, border) => {
+  return (
+    `padding: 7px 0;` +
+    `background: ${background};` +
+    `color: ${color};` +
+    `border-top: 1px solid ${border};` +
+    `border-bottom: 1px solid ${border}`
+  );
+};
+
+console.log(
+  '\n\n%c %c  Designed & Coded by Ian Espanto  %c \n\n\n',
+  consoleStyle('#E4C1F9', '#fff', '#D0B0E3'),
+  consoleStyle('#F694C1', '#FEF5F9', '#E087B0'),
+  consoleStyle('#E4C1F9', '#fff', '#D0B0E3')
+);
 
 ReactDOM.render(
   <React.StrictMode>
